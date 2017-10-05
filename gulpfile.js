4d5cc0da4275adef2faf1ff5883433128b987acd
Thai-Construct <thai@construct.sg>
2017-10-06 05:23:22 +0700

    Able to build bootstrap sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,71 @@
 /** require import **/
-var gulp = require('gulp');
-var jade = require('gulp-jade');
+const gulp = require('gulp');
+/* for jade */
+const jade = require('gulp-jade');
+/* for SASS */
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoPrefixer = require('autoprefixer');
+/* auto reload browser */
+const browserSync = require('browser-sync').create();
 
 /** variables **/
+/* general */
 const distDir = './dist';
 
+/* for jade - HTML pre-processors */
 const jadeConfig = {
   pageSourceFiles: './src/jade/pages/*.jade',
   allSourceFiles: './src/jade/**/*.jade',
   distDir: distDir
 }
 
+/* for sass - css pre-processors */
+const sassConfig = {
+  allSourceFiles: './src/sass/**/*.scss',
+  targetFile: './src/sass/all.scss',
+  distFileName: 'all.css',
+  distDir: distDir + "/css"
+}
 
+/* Settings */
+const siteProxy = "cojo.dev"
+
+/*** Gulp tasks ***/
+/* for sass */
+gulp.task('sass', function() {
+  return gulp.src(sassConfig.targetFile)
+  .pipe(sourcemaps.init())
+  .pipe(concat(sassConfig.distFileName))
+  .pipe(postcss([ autoPrefixer() ]))
+  .pipe(sass({outputstyle: 'compressed'}).on('error', sass.logError))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest(sassConfig.distDir))
+  .pipe(browserSync.stream({match: '**/*.css'}));
+});
+
+/* for jade */
 gulp.task('jade', function() {
   gulp.src(jadeConfig.pageSourceFiles)
   .pipe(jade({
     pretty: true
   }))
   .pipe(gulp.dest(jadeConfig.distDir))
-})
-
-gulp.task('watch', function() {
-  gulp.watch(jadeConfig.allSourceFiles, ['jade'])
 });
 
-gulp.task('default', ['jade', 'watch']);
+/* gulp - continue to watch the changes */
+gulp.task('watch', function() {
+  browserSync.init(null, {
+    proxy: 'http://' + siteProxy,
+    host: siteProxy,
+    browser: process.platform === 'linux' ? 'google-chrome' : 'google chrome',
+    open: 'external'
+  });
+  gulp.watch(jadeConfig.allSourceFiles, ['jade']);
+  gulp.watch(sassConfig.allSourceFiles, ['sass']);
+
+});
+/* default - help to only run 'gulp' in the command-line */
+gulp.task('default', ['jade', 'sass', 'watch']);
